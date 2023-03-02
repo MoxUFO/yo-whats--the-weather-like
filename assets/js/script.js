dcdac6aaacb66e79371b23581ad7729fa940a1ed
MOX UFO <mox.ufo@gmail.com>
2023-03-02 10:45:29 -0500

    redesign JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
 let locationButton = document.getElementById('button-parent')
 let upcomingweather = document.getElementById('card-parent')
 let locationSearch = document.getElementById('location-input')
-let saveLocationBtn = document.getElementById('submit-button ')
+let saveLocationBtn = document.getElementById('submit-button')
 
 
 
+function saveCity(event){
+event.preventDefault()
+  let storedCity = []
+let locationQuery = locationSearch.value
+storedCity.push(locationQuery)
+console.log(locationQuery)
+console.log(storedCity)
+// storedCity.push(locationQuery)
+// localStorage.setItem('query',storedCity)
 
+}
 
+for (let i = 0; i< localStorage.length; i++) {
 
-saveLocationBtn.addEventListener('click', function(){
-    let locationQuery = locationSearch.value
-    console.log(locationQuery)
-})
+  console.log(localStorage.city)
+}
+saveLocationBtn.addEventListener("click", saveCity)
