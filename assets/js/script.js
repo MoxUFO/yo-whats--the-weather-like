9e1b68fe5a01732385d988d40394fc112ab82265
MOX UFO <mox.ufo@gmail.com>
2023-03-02 16:52:19 -0500

    add function to fecth coordinates, and to fetch weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,42 @@ function displayHistory() {
   }
 }
 
+function getCoordinates(city) {
+  let geoFinderUrl =
+  "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=5&appid=c8cc486e034609223b1c1970df0b8ef2";
+  fetch(geoFinderUrl)
+  .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.length == 0) {
+        alert("please enter a valid city name");
+        return;
+      }
+      var usCity = data.find(function(element){
+        return element.country == "US"
+      })
+      if(usCity == undefined){
+        usCity = data[0]
+      }
+      saveCity(usCity.name)
+      searchForecast(usCity.lat, usCity.lon)
+    });
+}
+
+function searchForecast(lat,lon) {
+  console.log(lat, lon)
+  let queryUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=c8cc486e034609223b1c1970df0b8ef2";
+  //  lat={lat}&lon={lon}
+  let apiKey = "&appid=c8cc486e034609223b1c1970df0b8ef2";
+  fetch(queryUrl)
+  .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+    console.log(data)
+    })
+}
 
 
 document.getElementById("submit-button").addEventListener("click", handleFormSubmit);
